7d9c1a578e9ca7a02bbe5b194fcc74369467a004
Prateek saini <157352079+Prateek0000007@users.noreply.github.com>
2024-09-13 10:44:51 +0530

    HelloWorld.java

--- GFG7.java
+++ GFG7.java
@@ -1,31 +1,8 @@
-// Java Program to Swap Two values using third variable
-// using temp variable 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-// Importing generic libraries
-import java.util.*;
-
-class GFG7{
-
-    // Function to swap two numbers
-    // Using temporary variable
-    static void swapValuesUsingThirdVariable(int m, int n)
-    {
-        // Swapping the values
-        int temp = m;
-        m = n;
-        n = temp;
-        System.out.println("Value of m is " + m
-                           + " and Value of n is " + n);
-    }
-
-    // Main driver code
-    public static void main(String[] args)
-    {
-        // Random integer values
-        int m = 9, n = 5;
-
-        // Calling above function to
-        // reverse the numbers
-        swapValuesUsingThirdVariable(m, n);
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
     }
 }
